d45299d53ac0a26d73c6b2e7c7d9aae9c2b66896
Meegu80 <aceman80@nate.com>
2024-04-11 10:29:46 +0900

    Initial commit

---
+++ src/com/javalab/sec01/Human.java
@@ -0,0 +1,13 @@
+package com.javalab.sec01;
+
+public class Human {
+    // 필드(속성) : 정보 저장공간, human 이 가질 수 있는 성질
+    String ssn;
+    String name;
+    int age;
+
+    //생성자
+    public Human(){
+        System.out.println("여기는 Human 클래스 생성자");
+    }
+}
---
+++ src/com/javalab/sec01/HumanMain.java
@@ -0,0 +1,15 @@
+package com.javalab.sec01;
+
+
+public class HumanMain {
+    public static void main(String[] args) {
+
+
+        // Human 객체 생성
+        // Human : 도메인 클래스 이름
+        // human : 객체 참조 변수
+        // new : 객체 생성 연산자
+        // Human() : 생성자로 객체 초기화
+        Human human = new Human();
+    }
+}
